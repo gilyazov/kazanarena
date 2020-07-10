cb27675af2babe8aba315ea87eea61a471991831
gilyazov <gilyazov.aydar@gmail.com>
2020-07-10 15:46:04 +0300

    Интеграция с Uniteller

--- stadium/tour/index_standart.php
+++ stadium/tour/index_standart.php
@@ -1,14 +1,37 @@
 <div class="shBlock2 costItems">
- <figure> взрослым <strong>350</strong> рублей </figure> <figure class="pink"> детям <strong>300</strong>рублей</figure> <figure class="yellow"><br>
- <br>
- <br>
-	 индивидуальный<br>
-	 тур<br>
- <strong><br>
- </strong></figure>
+    <figure>
+        <?if ($USER->IsAdmin()):?>
+        <a href="#" class="btn js-buy-btn" data-item="adult" style="width: 100%; position: absolute; top: -30px; left: 0; transform: none">
+            <span>
+                <em style="transform: none">Купить</em>
+            </span>
+        </a>
+        <?endif;?>
+        <div>
+            взрослым <strong>350</strong> рублей
+        </div>
+    </figure>
+    <figure class="pink">
+        <?if ($USER->IsAdmin()):?>
+        <a href="#" class="btn js-buy-btn" data-item="child" style="width: 100%; position: absolute; top: -30px; left: 0; transform: none">
+            <span>
+                <em style="transform: none">Купить</em>
+            </span>
+        </a>
+        <?endif;?>
+        <div>
+            детям <strong>300</strong>рублей
+        </div>
+    </figure>
+    <figure class="yellow">
+        <div>
+            индивидуальный<br>
+            тур
+        </div>
+    </figure>
 </div>
- <br>
+<br>
 <p>
-	 Программа тура поможет ощутить особую атмосферу стадиона&nbsp;и увидеть: чашу стадиона, конференц-зал, VIP трибуны, зону представителей СМИ, зону игроков (раздевалки и тренировочные комнаты), зоны питания.<span>&nbsp; <br>
- <strong>Продолжительность: 1&nbsp;час</strong></span>
+    Программа тура поможет ощутить особую атмосферу стадиона&nbsp;и увидеть: чашу стадиона, конференц-зал, VIP трибуны, зону представителей СМИ, зону игроков (раздевалки и тренировочные комнаты), зоны питания.<span>&nbsp; <br>
+	<strong>Продолжительность: 1&nbsp;час</strong></span>
 </p>
